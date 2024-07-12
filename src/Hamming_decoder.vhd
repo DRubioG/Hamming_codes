846756007a058e818dd998a31c932c5b8625dc63
David R. <davidrubio97@outlook.com>
2024-07-12 20:27:00 +0200

    update

--- src/Hamming_decoder.vhd
+++ src/Hamming_decoder.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 use ieee.std_logic_misc.all;
 
 entity Hamming_decoder is
@@ -11,6 +12,8 @@ entity Hamming_decoder is
 end entity;
 
 architecture arch_Hamming_decoder of Hamming_decoder is
+
+constant C_value : std_logic_vector(15 downto 0) := (0=>'1', others=>'0');
 
 signal e : std_logic_vector(3 downto 0);
 signal flip_mask : std_logic_vector(15 downto 0);
@@ -24,7 +27,30 @@ begin
     e(2) <= code(3) xor code(4) xor code(5) xor code(6) xor code(11);
     e(3) <= code(7) xor code(8) xor code(9) xor code(10) xor code(11);
 
-    flip_mask <= (0=>'1', others=>'0') sll e;
+    
+    process(e)
+    begin
+        case e is
+            when x"0" => flip_mask <= C_value;
+            when x"1" => flip_mask <= C_value(15 downto 1)  & "0";
+            when x"2" => flip_mask <= C_value(15 downto 2)  & "00";
+            when x"3" => flip_mask <= C_value(15 downto 3)  & "000";
+            when x"4" => flip_mask <= C_value(15 downto 4)  & "0000";
+            when x"5" => flip_mask <= C_value(15 downto 5)  & "00000";
+            when x"6" => flip_mask <= C_value(15 downto 6)  & "000000";
+            when x"7" => flip_mask <= C_value(15 downto 7)  & "0000000";
+            when x"8" => flip_mask <= C_value(15 downto 8)  & "00000000";
+            when x"9" => flip_mask <= C_value(15 downto 9)  & "000000000";
+            when x"A" => flip_mask <= C_value(15 downto 10) & "0000000000";
+            when x"B" => flip_mask <= C_value(15 downto 11) & "00000000000";
+            when x"C" => flip_mask <= C_value(15 downto 12) & "000000000000";
+            when x"D" => flip_mask <= C_value(15 downto 13) & "0000000000000";
+            when x"E" => flip_mask <= C_value(15 downto 14) & "00000000000000";
+            when x"F" => flip_mask <= C_value(15) & "000000000000000";
+            when others => flip_mask <= (others=>'0');
+        end case;
+    end process;
+    
 
     corr_code <= code(11 downto 0) xor flip_mask(12 downto 1);
 
